e4e749e13d81893b14c47be8a7d6db541607d2f5
jfelix61 <rtfire456@hotmail.com>
2019-04-15 03:12:15 -0400

    .

--- app/main.py
+++ app/main.py
@@ -144,4 +144,4 @@ api.add_resource(calculate_ODE, '/calculateODE')
 api.add_resource(Graph, '/graph')
 
 if __name__ == "__main__":    
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=443)
